2559b54f46950febd62d53d9a8d5b267d7334609
grumpy-git <ldept@gnrs.co.uk>
2015-12-31 15:06:51 +0000

    Add sethostname module

---
+++ modules/sethostname/manifests/init.pp
@@ -0,0 +1,15 @@
+class sethostname {
+  file { "/etc/hostname":
+    ensure   => present,
+    owner    => root,
+    group    => root,
+    mode     => '0644',
+    content  => "$::hostname\n",
+    notify   => Exec["set-hostname"],
+  }
+  exec { "set-hostname":
+    command   => '/bin/hostname -F /etc/hostname',
+    unless    => "/usr/bin/test `hostname` = `/bin/cat /etc/hostname`",
+  } 
+}
+
